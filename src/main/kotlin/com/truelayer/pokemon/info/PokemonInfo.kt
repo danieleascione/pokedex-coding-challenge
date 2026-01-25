182c3d35adc46efd3ebeb798adc33aa89d0b51d9
Daniele Ascione <daniele.ascione@masabi.com>
2026-01-25 17:41:55 +0000

    refactor: replace wildcard imports in PokemonInfo with explicit imports

--- src/main/kotlin/com/truelayer/pokemon/info/PokemonInfo.kt
+++ src/main/kotlin/com/truelayer/pokemon/info/PokemonInfo.kt
@@ -1,7 +1,8 @@
 package com.truelayer.pokemon.info
 
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import kotlinx.serialization.Serializable
 
 @Serializable
